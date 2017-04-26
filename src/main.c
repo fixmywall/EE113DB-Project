0f9eea74c4e08e35decbf7e94461ead4253f8216
fixmywall <oliveryang95@gmail.com>
2017-04-26 15:09:40 -0700

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,244 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include "qdbmp.h"
+
+#define PI 3.14159
+#define BLACK_PIXEL 0
+#define WHITE_PIXEL 1
+
+typedef struct _BinaryDocument {
+	UCHAR* image;			// each element corresponds to one pixel for computation 
+	int background_color;	// 0 for white background, 1 for black background
+	int height;				// height of the image in pixels
+	int width;				// width of the image in pixels
+} BinaryDocument;
+
+// input_bmp: pointer to a BMP struct as defined by qdbmp.h
+UCHAR* ConvertImageToGrayscale(BMP* input_bmp, int height, int width) {
+	//allocate memory for output grayscale bitmap (1 byte per pixel)
+	UCHAR* bitmap_grayscale = (UCHAR*)malloc(sizeof(UCHAR) * height * width);
+
+	//convert RGB bmp image to grayscale using the luminosity 
+	int x, y;
+	for (x = 0; x < width; x++) {
+		for (y = 0; y < height; y++) {
+			UCHAR pixel_value = 0;
+			UCHAR r, g, b;
+			BMP_GetPixelRGB(input_bmp, x, y, &r, &g, &b);
+
+			if (BMP_GetBitsPerPixel(input_bmp) == 8) {
+				pixel_value = b;
+			}
+			else {
+				pixel_value = (UCHAR)(0.21*r + 0.72*g + 0.07*b);
+			}
+
+			bitmap_grayscale[x + y * width] = pixel_value;
+		}
+	}
+
+	//free input color image
+	BMP_Free(input_bmp);
+
+	return bitmap_grayscale;
+}
+
+//frees the members of a BinaryDocument struct
+void BinaryDocument_Free(BinaryDocument* doc) {
+	free(doc->image);
+}
+
+// Takes in a grayscale image and binarizes it (makes it black and white)
+// Uses Otsu's method, a global thresholding algorithm
+BinaryDocument Binarize(UCHAR* image, int height, int width) {
+	int total_pixels = height * width;		// total number of pixels in the grayscale image
+	int background_color;					// 0 for black, 1 for white background
+	int black_pixel_count = 0;				// count of black pixels in imgae
+	double total_intensity = 0; 			// sum of intensities of each pixel in the image
+	int histogram[256]; 					// histogram of the intensities of the pixels
+
+	int i;
+	for (i = 0; i < 256; i++) {				// set histogram array to zeros
+		histogram[i] = 0;
+	}
+
+	int t;
+	for (i = 0; i<total_pixels; i++) {
+		histogram[(int)image[i]]++; 		// populate the color histogram
+	}
+
+	for (i = 0; i < 256; i++) {
+		total_intensity += histogram[i] * i;
+	}
+
+	int optimal_threshold = 0;		// optimal threshold that will divide the pixels into background (dark) and foreground (light)
+	double w_b = 0;					// background weight: fraction of pixels in the background class
+	double w_f = 0; 				// foreground weight: fraction of pixels in the foreground class
+	double sum_b = 0; 				// sum of background intensities
+	double max_inter_var = 0; 		//maximum inter-class variance: defined as w_b * w_f * (u_b - u_f)^2
+
+									// iterate through all possible threshold values to find the optimal one
+									// the optimal threshold is the one that maximizes the between-class variance
+	for (t = 0; t < 256; t++) {		// for each possible threshold (0 to 255)
+		w_b += histogram[t];
+		sum_b += t * histogram[t]; 			// update sum of BG intensities
+		if (w_b == 0) continue; 			// if background is zero, go to next iteration
+
+		w_f = total_pixels - w_b;
+		if (w_f == 0) break;
+
+		double mean_b = sum_b / w_b;			// background mean: mean of pixel intensities in background class
+		double mean_f = (total_intensity - sum_b) / w_f;	// foreground mean
+		double inter_var = w_b * w_f * pow(mean_b - mean_f, 2); 	// inter-class variance for current threshold
+
+		// check if a new inter-class variance maximum has been found
+		// if so, update the optimal threshold to the current value of t
+		if (inter_var > max_inter_var) {
+			max_inter_var = inter_var;
+			optimal_threshold = t;
+		}
+	}
+
+	//apply global threshold to newly allocated output image
+	UCHAR* output_image = (UCHAR*)malloc(sizeof(UCHAR) * total_pixels);
+	for (i = 0; i<height*width; i++) {
+		int intens = image[i];
+		if (intens >= optimal_threshold) {
+			output_image[i] = WHITE_PIXEL; 			// set to white pixel (1 for binary image of 1 BPP)	
+		}
+		else {
+			output_image[i] = BLACK_PIXEL;			// set to black pixel (0 for binary image of 1 BPP)
+			black_pixel_count++;
+		}
+	}
+
+	// set background color to the majority color count
+	int white_pixel_count = total_pixels - black_pixel_count;
+
+	if (white_pixel_count > black_pixel_count) {
+		background_color = WHITE_PIXEL;
+	}
+	else background_color = BLACK_PIXEL;
+
+	//define output struct
+	BinaryDocument output_doc;
+	output_doc.background_color = background_color;
+	output_doc.height = height;
+	output_doc.width = width;
+	output_doc.image = output_image;
+
+	//free the input image
+	free(image);
+
+	return output_doc;
+}
+
+//checks for a skew angle and returns a new image array with the skew corrected
+int Deskew(UCHAR* binary_image, int height, int width) {
+
+}
+
+/*
+	THIS IS A NAIVE ROTATION ALGORITHM
+	SOON THIS WILL BE UPDATED TO IMPLEMENT THE "BLACK-RUNS" ALGORITHM,
+	WHICH REDUCES THE NUMBER OF HOLES IN THE OUTPUT IMAGE
+*/
+//binary_doc: pointer to BinaryDocument object to modify
+//angle_deg: rotation angle in degrees
+void Rotate(BinaryDocument* binary_doc, float angle_rad) {
+	int width = binary_doc->width;
+	int height = binary_doc->height;
+	int total_pixels = width * height;
+	int i;
+
+	//allocate memory for rotated image
+	UCHAR* output_image = malloc(sizeof(UCHAR) * total_pixels);
+
+	//fill output image with the background color
+	for (i = 0; i < total_pixels; i++) {
+		output_image[i] = binary_doc->background_color;
+	}
+
+	//iterate through the original image row-by-row, transforming each pixel to its rotated position
+	int x, y;
+	for (y = 0; y < binary_doc->height; y++) {
+		for (x = 0; x < binary_doc->width; x++) {
+			// check for foreground pixels
+			int pix_val = binary_doc->image[x + y * binary_doc->width];
+			if (pix_val != binary_doc->background_color) {
+				double sin_val, cos_val;
+				sin_val = sin(angle_rad);
+				cos_val = cos(angle_rad);
+
+				//calculate the rotated x and y values with respect to the origin (center of image)
+				int new_x = (x - width / 2) * cos_val - (y - height / 2) * sin_val;
+				int new_y = (x - width / 2) * cos_val + (y - height / 2) * sin_val;
+				
+				//check boundaries
+				int new_index = (new_x + width / 2) + (new_y + height / 2) * width;
+				if (new_index < 0 || new_index >= total_pixels) {
+					continue;
+				}
+
+				//transform the +/- x and y vals into array index
+				output_image[(new_x + width / 2)  + (new_y + height / 2) * binary_doc->width] = pix_val;
+			}
+		}
+	}
+
+	//deallocate original image
+	free(binary_doc->image);
+
+	//set new image
+	binary_doc->image = output_image;
+}
+
+
+
+//*****************************************************************************
+//
+// This is the main loop that runs the application.
+//
+//*****************************************************************************
+int main(void)
+{
+	BMP* bmp;
+	UCHAR* bitmap_grayscale;
+
+	//
+	//	Read a BMP file from the mass storage device
+	//
+	bmp = BMP_ReadFile("data/ocr_test_receipt.bmp");
+	BMP_CHECK_ERROR(stderr, -1); /* If an error has occurred, notify and exit */
+
+	int width, height;
+	width = BMP_GetWidth(bmp);
+	height = BMP_GetHeight(bmp);
+	
+	// convert the BMP data into grayscale UCHAR array
+	bitmap_grayscale = ConvertImageToGrayscale(bmp, height, width);
+
+	//convert to binary image
+	BinaryDocument binary_doc;
+	binary_doc = Binarize(bitmap_grayscale, height, width);
+
+	//rotate binary image
+	Rotate(&binary_doc, PI / 4);
+
+	//write output to file
+	FILE* fp;
+	fp = fopen("data/output.txt", "w");
+	int i;
+	for (i = 0; i < height*width; i++) {
+		fprintf(fp, "%d\n", (int)binary_doc.image[i]);
+	}
+	fclose(fp);
+	printf("Finished writing processed image.\n");
+
+	//free allocated memory
+	BinaryDocument_Free(&binary_doc);
+
+	return 0;
+}
